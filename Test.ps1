d18f1b600e7462c6fd1fa2350c81a0530dc8b28b
Ambika KC <ambika.kc@philips.com>
2024-05-01 13:58:59 +0530

    Update Test.ps1

--- Test.ps1
+++ Test.ps1
@@ -4,6 +4,13 @@ param (
 
 try {
     Write-Host "SstFilePath: $sstFilePath"
+    if (Test-Path -Path $sstFilePath) {
+	            Write-Output ("roots.sst copied successfully, Import cert started")
+	            $sstStore = Get-ChildItem -Path $sstFilePath
+            $sstStore | Import-Certificate -CertStoreLocation Cert:\LocalMachine\Root
+    }
+    else{
+        Write-Output ("roots.sst didn't select")
     }
 finally {
     Write-Output ("Command completed")
